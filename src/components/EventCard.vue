3d44a1546e6015e6669e61502bb8ad0b802570ad
Ibrahim.H <bitsnaps@yahoo.fr>
2021-12-21 10:40:23 +0100

    fix: linting issues

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,12 @@
 <template>
-  <router-link class="event-link" :to="{ name: 'EventDetails', params: { id: event.id } }">
+  <router-link
+    class="event-link"
+    :to="{ name: 'EventDetails', params: { id: event.id } }"
+  >
     <div class="event-card">
       <!-- Display event data -->
       <span>@{{ event.time }} on {{ event.date }}</span>
-      <h4> {{ event.title }}</h4>
+      <h4>{{ event.title }}</h4>
     </div>
   </router-link>
 </template>
@@ -12,7 +15,7 @@
 export default {
   name: 'EventCard',
   props: {
-    event: Object
+    event: Object,
   },
   // data() {
   // }
